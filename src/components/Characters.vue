c6284632f809c85714cb1d7e9c035ff541e2dcb6
Jake Robbins <jrobbinsd@gmail.com>
2020-01-09 20:18:26 -0700

    add character

---
+++ src/components/Characters.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+	<div class="">
+	this is going to be the character selection component	
+	</div>
+
+</template>
+
+<script lang='ts'>
+export default {
+
+}
+</script>
+
+<style lang="css" scoped>
+</style>
