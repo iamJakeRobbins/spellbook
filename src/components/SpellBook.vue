21bdd3664f0ca376384b9a861fe2fc70063f52f8
Jake Robbins <jrobbinsd@gmail.com>
2020-01-09 20:20:30 -0700

    add book component

---
+++ src/components/SpellBook.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+<div class="">
+	this will be the spellbook component
+</div>
+</template>
+
+<script lang='ts'>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
