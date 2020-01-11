30360febe87520ffb0be37046268dd9de8475460
Jake Robbins <jrobbinsd@gmail.com>
2020-01-11 14:32:28 -0700

    add replacement home component

---
+++ src/routes/Home.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="">
+  	This is our new home page and we're going to replace the views folder completely
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
