76f3e5ff9a2ccc70af5a5dd3be80a265a16288c2
Jake Robbins <jrobbinsd@gmail.com>
2020-01-11 09:27:34 -0700

    navbar basics

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-					<page-header></page-header>
+		<page-header></page-header>
 		<!-- <div class="container-fluid appbackground"> -->
 
 
@@ -52,16 +52,5 @@ export default class App extends Vue {}
 	/* height: 100vh; */
 	width: 80%;
 	background-color: white;
-}
-.header{
-	position:fixed; /* fixing the position takes it out of html flow - knows
-                   nothing about where to locate itself except by browser
-                   coordinates */
-  left:0;           /* top left corner should start at leftmost spot */
-  top:0;            /* top left corner should start at topmost spot */
-  width:100vw;      /* take up the full browser width */
-  z-index:200;  /* high z index so other content scrolls underneath */
-  height:100px;
-
 }
 </style>
