75a3b8233c8b10dd7d60e7c7f972aae18151c87b
Jake Robbins <jrobbinsd@gmail.com>
2020-01-11 14:33:24 -0700

    remove unused views folder

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,10 +15,6 @@ const routes = [
     path: '/char',
     name: 'CharacterChoice',
 		component: ChooseCharacter,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
 ];
 
