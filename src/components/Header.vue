ad638b5b7a4a907e8388ee5893de6bb39cf5c39c
Jake Robbins <jrobbinsd@gmail.com>
2020-01-11 16:19:04 -0700

    add home button to nav for ease of in ui navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
     <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home</a>
+                <router-link to="/">Home</router-link>
             </li>
 
         </ul>
