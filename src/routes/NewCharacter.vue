28d94ac198e7bb5aeea99e87e5336f1ccf2adc5a
Jake Robbins <jrobbinsd@gmail.com>
2020-01-14 21:26:23 -0700

    need to finish post request

--- src/routes/NewCharacter.vue
+++ src/routes/NewCharacter.vue
@@ -13,7 +13,7 @@
 			<input type="text" name="" :value='charClass'>
 		</div>
 		<div class="aRow">
-			<button class="btn btn-primary">Add Character</button>
+			<button class="btn btn-primary" :value="charId" @click='submitCharacter'>Add Character</button>
 		</div>
   </div>
 </template>
@@ -26,6 +26,42 @@ export default class NewCharacter extends Vue {
 	name:string = '';
 	charClass:string = '';
 	level:number = 0;
+	charId:any =  null;
+	url: string = `http://localhost:3000/`;
+
+	submitCharacter(e)
+	{
+		if(e.target.value) {
+			this.updateCharacter();
+		} else {
+			this.submitCharacter();
+		}
+	}
+
+	async submitCharacter()
+	{
+		let data = {
+			name: this.name,
+			level: this.level,
+			class: this.charClass,
+		}
+		let request = await fetch(`${this.url}/submitCharacter`, {
+		  method: 'POST', // or 'PUT'
+		  headers: {
+		    'Content-Type': 'application/json',
+		  },
+		  body: JSON.stringify(data),
+		})
+
+}
+
+
+	updateCharacter()
+	{
+		// doesn't need to do anything for now;
+		console.log('we in it')
+	}
+
 	mounted()
 	{
 		console.log('here');
