2c2dee0a4339daf9dadce48b7288b598613d8bdf
Jake Robbins <jrobbinsd@gmail.com>
2020-01-14 20:29:58 -0700

    new route

---
+++ src/routes/NewCharacter.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="">
+  	hello world
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class NewCharacter extends Vue {
+	mounted()
+	{
+		console.log('here');
+	}
+}
+</script>
+
+<style lang="css" scoped>
+</style>
