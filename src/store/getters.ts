77626e90041e2879885fda956931941f3254d3b8
Jake Robbins <jrobbinsd@gmail.com>
2020-01-25 08:53:27 -0700

    getters deserve their own file

---
+++ src/store/getters.ts
@@ -0,0 +1,15 @@
+
+export const getters =  {
+  selectedCharacter: (state) => {
+    return state.selectedCharacter;
+  },
+      characters: (state) => {
+    return state.characters;
+  },
+      classes: (state) => {
+    return state.classes;
+  },
+      url: (state) => {
+    return state.url;
+  },
+};
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,27 +2,14 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import {state} from "@/store/state";
 import {mutations} from "@/store/mutations";
+import {getters} from "@/store/getters";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state,
   mutations,
-
-	getters: {
-  	selectedCharacter: (state) => {
-  		return state.selectedCharacter;
-		},
-		characters: (state) => {
-  		return state.characters;
-		},
-		classes: (state) => {
-			return state.classes;
-		},
-		url: (state) => {
-  		return state.url;
-		},
-	},
+	getters,
   actions: {
   },
   modules: {
